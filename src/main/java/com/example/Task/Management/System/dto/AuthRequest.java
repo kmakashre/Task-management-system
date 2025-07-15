23fcabfdddb685f85de814455948bd44f7a29494
HP <karishmamakashre@gmail.com>
2025-07-15 16:07:46 +0530

    user login input

---
+++ src/main/java/com/example/Task/Management/System/dto/AuthRequest.java
@@ -0,0 +1,16 @@
+package com.example.Task.Management.System.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
