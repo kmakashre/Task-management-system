52fabfcf62fd96975ed77c3d26629ae8d57623af
HP <karishmamakashre@gmail.com>
2025-07-15 16:02:58 +0530

    customizing API documentation settings

---
+++ src/main/java/com/example/Task/Management/System/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.example.Task.Management.System.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info().title("Task Management API").version("1.0"))
+                .components(new Components().addSecuritySchemes("bearer-key",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearer-key", List.of("read", "write"))
+                );
+    }
+}
