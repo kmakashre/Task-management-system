55d7d5f7d99eb24e5a5cfc05cd99d9ffd4fcd44a
HP <karishmamakashre@gmail.com>
2025-07-15 16:01:17 +0530

    Add OpenApiConfig to define Swagger security schemes

---
+++ src/main/java/com/example/Task/Management/System/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.example.Task.Management.System.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@SecurityRequirement(name = "BearerAuth")
+public class OpenApiConfig {
+}
