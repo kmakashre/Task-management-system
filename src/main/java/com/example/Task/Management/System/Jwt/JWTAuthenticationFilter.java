3a46c84905174176cfd91a7ceafbda84d0b39412
HP <karishmamakashre@gmail.com>
2025-07-15 16:14:58 +0530

    extract and validate JWT from requests

---
+++ src/main/java/com/example/Task/Management/System/Jwt/JWTAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.example.Task.Management.System.Jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    private JwtHelper jwtHelper;
+    private UserDetailsService userDetailsService;
+    private static final Logger log = LoggerFactory.getLogger(JWTAuthenticationFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestHeader = request.getHeader("Authorization");
+        log.info("Header: {}", requestHeader);
+
+        String username = null;
+        String token = null;
+
+        if (requestHeader != null && requestHeader.startsWith("Bearer ")) {
+            token = requestHeader.substring(7);
+            try {
+                username = jwtHelper.getUsernameFromToken(token);
+            } catch (Exception e) {
+                log.warn("Invalid token", e);
+            }
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (jwtHelper.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
